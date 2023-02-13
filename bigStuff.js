0f3e4bab8bf7a1ddf8c5cea502ce3bb69c6485ae
dchamp16 <dchamp16@gmail.com>
2023-02-13 00:07:10 -0700

    continue

--- bigStuff.js
+++ bigStuff.js
@@ -1,18 +1,10 @@
-// calculating block (size / 4096)
-// use / so it works for everything
-
-//TODO seperate file give the extension
-//readme.txt txt
-//.gitignore gitignore
-//music '' << if no extension
-//document ''
-
 async function checkFile(path) {
   const fs = require("fs");
   const filesize = require("filesize");
 
   let filesFolders = fs.readdirSync(path);
-  let arrayFile = [{}];
+  let arrayFiles = [];
+  let temArr;
   /*
 regex check string if it has extension name
 example: true
@@ -33,61 +25,92 @@ example: false
           console.log(error.message);
         } else {
           //checks if it a file
-          if (stats.isDirectory() && !regexExtensionName.test(value)) {
-            fs.stat(value + "/", (error, folder) => {
-              arrayFile.push({
+          if (stats.isFile && regexExtensionName.test(value)) {
+            // console.log("file:", value);
+            /* end test */
+            fs.stat(value, (error, folder) => {
+              arrayFiles.push({
                 name: value,
                 sizeNum: folder.size,
                 sizeStr: filesize.filesize(stats.size),
                 isDirectory: folder.isDirectory(),
                 blocks: Math.ceil(stats.size / 4096),
-                files: [],
               });
             });
             //checkk if its a file
-          } else {
-            arrayFile.push({
+          } else if (stats.isDirectory && !regexExtensionName.test(value)) {
+            // console.log("direcory:", value);
+            /* end test */
+            arrayFiles.push({
               name: value,
               sizeNum: stats.size,
               sizeStr: filesize.filesize(stats.size),
               isFile: stats.isFile(),
               blocks: Math.ceil(stats.size / 4096),
+              files: [],
             });
+
+            // console.log(folderFiles);
           }
         }
-        const fs.readdirSync()
-
-
-        for (let directory of directories) {
-          fs.stat(`${__dirname}/${value}/${directory}`, (error, subFile) => {
-            // console.log(subFile);
-            console.log(arrayFile);
-            // arrayFile[0].files.push({
-            //   name: directory,
-            //   sizeNum: subFile.size,
-            //   sizeStr: filesize.filesize(subFile.size),
-            //   isDirectory: subFile.isDirectory(),
-            //   blocks: Math.ceil(stats.size / 4096),
-            // });
-          });
-        }
-        arrayFile.sort((a, b) => a.sizeNum - b.sizeNum);
+        arrayFiles.sort((a, b) => a.sizeNum - b.sizeNum);
       });
     }
+
+    let folders = fs.readdirSync(`.`);
+    temArr = []; //TODO: change name
+    for (let file of folders) {
+      fs.stat(file, (error, subfile) => {
+        if (subfile.isDirectory() && !regexExtensionName.test(subfile)) {
+          let files = `${__dirname}/${file}`;
+          let foldersWithFile = fs.readdirSync(files);
+          foldersWithFile.forEach((data) => {
+            // console.log(file);
+            // if() //check if the extension same
+            fs.stat(`${files}/${data}`, (err, fileStat) => {
+              temArr.push({
+                name: data,
+                directoryName: file,
+                sizeNum: fileStat.size,
+                sizeStr: filesize.filesize(fileStat.size),
+                isFile: fileStat.isFile(),
+                blocks: Math.ceil(fileStat.size / 4096),
+              });
+            });
+          });
+        }
+      });
+    }
+    setTimeout(() => {
+      // console.log(temArr);
+    }, 100);
   } catch (error) {
     console.log(error.message);
   }
+
+  //compare 2 object if both
+  function compare(obj1, obj2) {
+    for (let first in obj1) {
+      for (let second in obj2) {
+        if (obj1[first].name === obj2[second].directoryName) {
+          return (obj1[first].files = obj2);
+        }
+      }
+    }
+  }
+
   await setTimeout(() => {
-    // console.log(arrayFile);
-  }, 100);
+    compare(arrayFiles, temArr);
+    console.log(arrayFiles);
+  }, 2000);
 }
 
 checkFile(".");
 
 /* clear the console in 10 second */
-setTimeout(() => {
-  console.clear();
-}, 15_000);
+// setTimeout(() => {
+//   console.clear();
+// }, 15_000);
 
 function usage() {
   //TODO print the help screen
--- test.js
+++ test.js
@@ -1,16 +1,96 @@
-const fs = require("fs");
+let obj1 = [
+  {
+    name: "node_modules",
+    sizeNum: 128,
+    sizeStr: "128 B",
+    isFile: false,
+    blocks: 1,
+    files: [],
+  },
+  {
+    name: "remove_this",
+    sizeNum: 128,
+    sizeStr: "128 B",
+    isFile: false,
+    blocks: 1,
+    files: [],
+  },
+  {
+    name: "test",
+    sizeNum: 128,
+    sizeStr: "128 B",
+    isFile: false,
+    blocks: 1,
+    files: [],
+  },
+  {
+    name: ".git",
+    sizeNum: 512,
+    sizeStr: "512 B",
+    isDirectory: true,
+    blocks: 1,
+  },
+  {
+    name: "bigStuff.js",
+    sizeNum: 3174,
+    sizeStr: "3.17 kB",
+    isDirectory: false,
+    blocks: 1,
+  },
+];
 
-let filesAndFolders = fs.readdirSync(`remove_this/`);
+let obj2 = [
+  {
+    name: "filesize",
+    directoryName: "node_modules",
+    sizeNum: 224,
+    sizeStr: "224 B",
+    isFile: false,
+    blocks: 1,
+  },
+  {
+    name: ".package-lock.json",
+    directoryName: "node_modules",
+    sizeNum: 409,
+    sizeStr: "409 B",
+    isFile: true,
+    blocks: 1,
+  },
+  {
+    name: "League Website.pptx",
+    directoryName: "remove_this",
+    sizeNum: 20292884,
+    sizeStr: "20.29 MB",
+    isFile: true,
+    blocks: 4955,
+  },
+  {
+    name: "json.txt",
+    directoryName: "remove_this",
+    sizeNum: 16120,
+    sizeStr: "16.12 kB",
+    isFile: true,
+    blocks: 4,
+  },
+  {
+    name: "blah.json",
+    directoryName: "test",
+    sizeNum: 158,
+    sizeStr: "158 B",
+    isFile: true,
+    blocks: 1,
+  },
+];
 
-console.log(filesAndFolders);
-// let roots = fs.readdirSync(".");
-
-for (let root of filesAndFolders) {
-  fs.stat(root, (error, stats) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(root, stats);
+function compare(obj1, obj2) {
+  for (let one in obj1) {
+    for (let two in obj2) {
+      if (obj1[one].name === obj2[two].directoryName) {
+        return (obj1[one].files = obj2);
+      }
     }
-  });
+  }
 }
+
+compare(obj1, obj2);
+console.log(obj1);
