a1e02732da6f5ec3fc0c0b3f2d595107301b5cca
Peter Ramos <dchamp16@gmail.com>
2023-03-02 13:38:35 -0700

    update and added chalk

--- test.js
+++ test.js
@@ -1,11 +1,3 @@
-function myFunction(input) {
-  if (input >= 10) {
-    //if input is greater than 10
-    console.log("done"); // show num value
-  } else {
-    input++;
-    myFunction(input);
-  }
-  console.log(input);
-}
-myFunction(0);
+import chalk from "chalk";
+
+console.log(chalk.blue("Hello world!"));
