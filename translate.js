e7581a69556afda5426fe8769dc9b4ea42dee4f7
dchamp16 <dchamp16@gmail.com>
2023-03-16 00:23:33 -0600

    added some code for trasnlate for reference and updated the bigstuffv2.txt

--- translate.js
+++ translate.js
@@ -1,4 +1,20 @@
-new google.translate.TranslateElement(
-  { pageLanguage: "en" },
-  "google_translate_element"
-);
+const fs = require("fs");
+const msgs = require("./msgs.en-us.json");
+const mensahe = require("./msgs.es-MX.json");
+
+console.log(msgs["must supply a path1"]);
+return;
+
+let userInput = process.argv.slice(2);
+const language = userInput[0];
+const location = userInput[1];
+
+// console.log(`lang: ${language} | loc: ${location}`); //for testing
+
+function helpAnyLang(lang, loc) {
+  const text = fs.readFileSync(`help-${lang}-${loc}.txt`, "utf-8");
+
+  console.log(text);
+}
+
+helpAnyLang(language, location);
