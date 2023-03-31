480e7eeb5784719ebb604f086caf81fcd637377f
Peter Ramos <86265240+dchamp16@users.noreply.github.com>
2023-03-31 09:28:43 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,32 +1,15 @@
-const fs = require("fs");
-const puppeteer = require("puppeteer");
+const puppeteer = require('puppeteer');
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  await page.goto('https://example.com');
 
-  for (let i = 1; i <= 100; i++) {
-    let url = `https://www.passwordrandom.com/most-popular-passwords/page/${i}`;
-    await page.goto(url);
-    console.log(url);
+  const tds = await page.$$('td'); // select all td elements
 
-    const data = await page.evaluate(() => {
-      const tds = Array.from(
-        document.querySelectorAll("table tr td:nth-child(2)")
-      );
-      return tds.map((td) => td.innerText);
-    });
-
-    let arrayPasswords = [];
-
-    data.forEach((password, key) => {
-      arrayPasswords.push({ page: `Page-${i}`, key: key, password: password });
-    });
-    fs.appendFile(
-      "scrapePassword.json",
-      JSON.stringify(arrayPasswords, null, 2),
-      (err) => (err ? console.log(err) : console.log(`Page ${i} done`))
-    );
+  for (let i = 0; i < tds.length; i++) {
+    const tdContent = await tds[i].getProperty('textContent');
+    console.log(await tdContent.jsonValue());
   }
 
   await browser.close();
