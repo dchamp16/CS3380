34ca05582eab3d66d19ec14680fded8503f6400f
dchamp16 <dchamp16@gmail.com>
2023-03-31 23:08:04 -0600

    last check what else do i need

--- scrapePws.js
+++ scrapePws.js
@@ -4,25 +4,41 @@ const puppeteer = require("puppeteer");
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  let objPass = {}
+  let objPass = {};
   let arrPassContainer = [];
-  for (let i = 1; i <= 5; i++) {
+  let spinner = makeCounter();
+  for (let i = 1; i <= 100; i++) {
+    spinner.tick();
     let url = `https://www.passwordrandom.com/most-popular-passwords/page/${i}`;
     await page.goto(url);
-    console.log(`${url} Printing`);
+    // console.log(`${url} Printing`);
     const tdElements = await page.$$eval("table tr td:nth-child(2)", (tds) =>
       tds.map((td) => td.textContent.trim())
     );
     arrPassContainer.push(tdElements);
+    spinner.clear();
   }
-  let flattendArr = arrPassContainer.flat()
-  let passwords = Object.assign(objPass, flattendArr)
+  let flattendArr = arrPassContainer.flat();
+  flattendArr.sort();
+  let passwords = Object.assign(objPass, flattendArr);
 
-  // console.log(passwords);
-  fs.writeFi("mcupws.json", JSON.stringify(passwords), (err) =>
+  fs.writeFileSync("mcupws.json", JSON.stringify(passwords), (err) =>
     err ? console.log(err) : console.log(`Print done`)
   );
-  // console.log(arrPassContainer);
 
   await browser.close();
 })();
+
+function makeCounter() {
+  const spinningChars = `|/-\\`;
+  let n = 0;
+  function clear() {
+    process.stdout.write(`\r`);
+  }
+  function tick() {
+    let animate = `\r${spinningChars[n % spinningChars.length]}`;
+    if (n % 3 === 0) process.stdout.write(`${animate}LOADING${animate}`);
+    n++;
+  }
+  return { tick, clear };
+}
