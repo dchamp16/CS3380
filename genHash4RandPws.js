624c4b79f1f72311a5c4d74abb7389c2595d936c
dchamp16 <dchamp16@gmail.com>
2023-04-10 00:35:12 -0600

    update

--- genHash4RandPws.js
+++ genHash4RandPws.js
@@ -4,6 +4,7 @@ const _ = require("lodash");
 let arrPass = [];
 
 fs.readFile("mcupws.json", "utf8", (error, data) => {
+  console.time("print hash");
   if (error) {
     console.error(error);
   }
@@ -33,30 +34,38 @@ fs.readFile("mcupws.json", "utf8", (error, data) => {
     hashes.push(hash);
   }
   const shuffledHashes = _.shuffle(hashes);
+
   fs.writeFile("PeterRamos.2K.hashes.txt", shuffledHashes.join("\n"), (err) => {
     if (err) throw err;
     console.log("The file has been saved!");
   });
+  console.timeEnd("print hash");
 });
 
 /* generator function random char */
 function* alphanumericGenerator() {
-  const chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+  const chars = /a-zA-Z0-9/;
 
   // Produce 390 characters of 1 alphanumeric
 
   for (let i = 0; i < 390; i++) {
     let str = "";
-    str += chars[Math.floor(Math.random() * chars.length)];
+    str += String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+    str += String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+    str += String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+    str = str.charAt(Math.floor(Math.random() * 3));
     yield str;
   }
 
   // Produce 100 characters of 2 alphanumeric
-
-  for (let i = 0; i < 99; i++) {
+  for (let i = 0; i < 100; i++) {
     let str = "";
     for (let j = 0; j < 2; j++) {
-      str += chars[Math.floor(Math.random() * chars.length)];
+      str += chars.test(
+        String.fromCharCode(Math.floor(Math.random() * 62) + 48)
+      )
+        ? String.fromCharCode(Math.floor(Math.random() * 62) + 48)
+        : String.fromCharCode(Math.floor(Math.random() * 25) + 97);
     }
     yield str;
   }
@@ -65,7 +74,11 @@ function* alphanumericGenerator() {
   for (let i = 0; i < 10; i++) {
     let str = "";
     for (let j = 0; j < 3; j++) {
-      str += chars[Math.floor(Math.random() * chars.length)];
+      str += chars.test(
+        String.fromCharCode(Math.floor(Math.random() * 62) + 48)
+      )
+        ? String.fromCharCode(Math.floor(Math.random() * 62) + 48)
+        : String.fromCharCode(Math.floor(Math.random() * 25) + 97);
     }
     yield str;
   }
