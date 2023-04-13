df8b597a465053b7ef614f5ce6cc5181af28a7b7
Peter Ramos <dchamp16@gmail.com>
2023-04-13 14:12:21 -0600

    update

--- cluster.js
+++ cluster.js
@@ -1,12 +1,31 @@
-console.log("\nloading");
 const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
 
 if (cluster.isMaster) {
-  // when you first run this file, the code runs as master
-  console.log("Master");
-  cluster.fork(); // first worker, it will run this same file from the top as a worker
-  cluster.fork(); // second worker, it will run this same file from the top as a worker
-} else if (cluster.isWorker) {
-  console.log("Worker");
-  process.exit();
+  const args = process.argv.slice(2).map(Number);
+  const [start, end] = args;
+  console.log("Master: I have work to do from %d to %d", start, end);
+  console.log("Master: I will get each worker to do a portion");
+  const range = end - start;
+  const step = range / numCPUs;
+  for (let i = 0; i < numCPUs; i++) {
+    const worker = cluster.fork();
+    worker.send({
+      id: i,
+      start: start + i * step,
+      end: start + i * step + step - 1,
+    });
+    // Master tells each worker what portion of the work to do
+  }
+} else {
+  //Worker
+  process.on("message", (order) => {
+    console.log(
+      "Worker %d: I have work to do from %d to %d",
+      order.id,
+      order.start,
+      order.end
+    );
+    process.exit();
+  });
 }
