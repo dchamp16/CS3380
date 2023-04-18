d4955b2ca95e76e6f769c66aba1a928fb4b4be62
dchamp16 <dchamp16@gmail.com>
2023-04-18 00:45:49 -0600

    update

--- cluster.js
+++ cluster.js
@@ -2,30 +2,28 @@ const cluster = require("cluster");
 const numCPUs = require("os").cpus().length;
 
 if (cluster.isMaster) {
-  const args = process.argv.slice(2).map(Number);
-  const [start, end] = args;
-  console.log("Master: I have work to do from %d to %d", start, end);
-  console.log("Master: I will get each worker to do a portion");
-  const range = end - start;
-  const step = range / numCPUs;
+  const [, , qty] = process.argv.map(Number);
+  const splitQty = qty / numCPUs;
+
+  console.log("Master: I have %d things to do", qty);
+  console.log("Master: I will get my workers to do it for me.");
+
   for (let i = 0; i < numCPUs; i++) {
     const worker = cluster.fork();
-    worker.send({
-      id: i,
-      start: start + i * step,
-      end: start + i * step + step - 1,
-    });
-    // Master tells each worker what portion of the work to do
+    let msg = { id: i, start: i * splitQty, end: i * splitQty + splitQty };
+    console.log(msg);
+    worker.send(msg);
+    worker.on("message", (text) =>
+      console.log('Master: my minion says "%s"', text)
+    );
   }
 } else {
-  //Worker
-  process.on("message", (order) => {
-    console.log(
-      "Worker %d: I have work to do from %d to %d",
-      order.id,
-      order.start,
-      order.end
-    );
+  //worker
+  process.on("message", ({ id, start, end }) => {
+    console.log(`Worker ${id}: I have work to do from ${start} to ${end}`);
+
+    // Sometime later ...
+    process.send(`Worker ${id} all done with my ${end - start + 1} things`);
     process.exit();
   });
 }
