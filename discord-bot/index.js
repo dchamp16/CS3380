4ab984fed9718e8d22e728fb485d2838f136952c
dchamp16 <dchamp16@gmail.com>
2023-04-22 23:12:33 -0600

    update

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -1,8 +1,11 @@
-import { Client, GatewayIntentBits } from "discord.js";
+import { Client, GatewayIntentBits, Routes } from "discord.js";
 import { config } from "dotenv";
+import { REST } from "@discordjs/rest";
 
 // turn on dotenv
 config();
+const BOTTOKEN = process.env.BOT_TOKEN; // gets the bot token
+const CLIENTID = process.env.CLIENT_ID;
 
 const client = new Client({
   intents: [
@@ -12,15 +15,38 @@ const client = new Client({
   ],
 });
 
-const TOKEN = process.env.BOT_TOKEN; // gets the bot token
+const rest = new REST({ version: "10" }).setToken(BOTTOKEN);
 
-client.login(TOKEN);
+client.on("ready", () => console.log(`${client.user.tag} has log in `));
 
-client.on("ready", () => {
-  console.log(`${client.user.tag} has log in `);
+client.on("interactionCreate", (interaction) => {
+  if (interaction.isChatInputCommand()) {
+    console.log("hello world");
+    interaction.reply({ content: "konichiwa" });
+  }
 });
 
-client.on("messageCreate", (message) => {
-  let foo = message.content === "peter" ? true : false;
-  console.log(foo);
-});
+async function main() {
+  const commands = [
+    {
+      name: "order",
+      description: "Order soemthing...",
+    },
+    {
+      name: "tutorialhelp2",
+      description: "Help Tutorial",
+    },
+    {
+      name: "tutorialhelp3",
+      description: "Help Tutorial",
+    },
+  ];
+  try {
+    console.log("Started refreshing application (/) commands.");
+    await rest.put(Routes.applicationCommands(CLIENTID), { body: commands });
+    client.login(BOTTOKEN);
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+main();
